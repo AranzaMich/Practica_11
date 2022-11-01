7fbe8f3924c8548712b0b557f656ac794145e898
AranzaMich <aranzamichelle1@hotmail.com>
2022-10-31 22:39:51 -0600

    Practica

---
+++ actividad.js
@@ -0,0 +1,43 @@
+function Vacio(inputField) {
+    if (inputField.type === "checkbox") {
+	      if(inputField.checked)
+	          return false;
+	      return true;
+    }
+    if (inputField.value === "" ) {
+	      return true;
+    }
+    return false;
+}
+
+function Fondo(inputDiv){
+  inputDiv.style.backgroundColor = 'red';
+	inputDiv.parentNode.style.backgroundColor = 'red';
+	inputDiv.parentNode.style.color = 'white';
+}
+
+function Borrar(inputDiv){
+	inputDiv.parentNode.style.backgroundColor = 'black';
+	inputDiv.parentNode.style.color = 'white';
+}
+
+
+window.onload = function(){
+    var form = document.getElementById("form");
+    var message = document.getElementById("message");
+      form.onsubmit = function(e){
+            var requiredInputs = document.querySelectorAll(".required");
+            for (var i=0; i < requiredInputs.length; i++){
+                if(Vacio(requiredInputs[i]) ){
+                  message.innerHTML = "<b>Error</b> Debes proporcionar todos los campos marcados como obligatorios";
+                  message.style.backgroundColor = 'rgb(255, 105, 114)';
+                  message.style.border = '2px solid rgb(255, 105, 114)';
+                  e.preventDefault();
+                      Fondo(requiredInputs[i]);
+                }
+                else{
+                      Borrar(requiredInputs[i]);
+                }
+            }
+      }
+  }
